b95e240620f2fb6dd3da3f848685b75278dc986c
zbihari <bihari.zoltan@fonixit.hu>
2022-02-01 16:15:09 +0100

    Refact to multi project and add gateway project

---
+++ gym-gateway/src/main/java/hu/byhi/example/gym/GymGatewayApplication.java
@@ -0,0 +1,13 @@
+package hu.byhi.example.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymGatewayApplication.class, args);
+	}
+
+}
---
+++ gym-gateway/src/test/java/hu/byhi/example/gym/GymGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.byhi.example.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
